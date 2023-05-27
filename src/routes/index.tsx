3d1e4e1f724d7b927445c607e26b2558d36722db
Andres Arcila <andresarcila2@me.com>
2023-05-27 00:35:52 -0500

    ejeccuting file doc on click

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,34 +1,31 @@
-/**
- * eCloud4 Backend service - services for app and bo of eCloud4
- * @fileoverview index.tsx
- * @description 
- * @version 1.0.0
- * @date Fri May 26 2023 22:58:47 GMT-0500 (hora estándar de Colombia)
- * @company eCloud4 Corporation
- *
- * This software is the property of eCloud4 Corporation. Unauthorized reproduction or distribution of
- * this software, or any portion of it, may result in severe civil and criminal penalties, and will be
- * prosecuted to the maximum extent possible under the law.
- *
- * @license MIT
- * @copyright Copyright (c) 2023 eCloud4
- * @license eCloud4 Corporation Software License
- */
-import { component$} from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$, useStore } from '@builder.io/qwik';
+import { DocumentHead, server$ } from '@builder.io/qwik-city';
 import path from 'path';
 import { processFilesInFolder } from '~/services/addHeadersToFiles';
 
 export default component$(() => {
-  const folder = path.resolve('src');
-  const filePattern = /\.tsx$/;
-
-  //processFilesInFolder(folder,filePattern);
+  const store = useStore({
+    folder: ''
+  })
+  const processFiles = server$(()=>{
+    if(store.folder != ''){
+      const folder = path.resolve(store.folder);
+      const filePattern = /\.tsx$/;
+      console.log(folder,filePattern);
+      processFilesInFolder(folder,filePattern);
+    }
+  })
 
   
   return (
     <>
-      <button >processFilesInFolder</button>
+      <input type="text" 
+      value={store.folder}
+      onInput$={(ev) => (store.folder = (ev.target as HTMLInputElement).value)}
+      />
+      <button onClick$={async ()=>{
+        await processFiles();
+        }} >processFilesInFolder</button>
     </>
   );
 });
